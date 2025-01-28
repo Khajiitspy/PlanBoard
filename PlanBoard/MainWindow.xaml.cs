229e52ff6314c65a085a208eec43c9fe6f11fdec
Khajiitspy <150380340+Khajiitspy@users.noreply.github.com>
2025-01-28 13:11:33 -0500

    Add project files.

---
+++ PlanBoard/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using PlanBoard.Tools;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace PlanBoard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool AddNoteMode = false;
+        string BoardFolder = "SavedBoards";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            FillLoadMenu();
+        }
+
+        private void SaveBoard_Click(object sender, RoutedEventArgs e)
+        {
+            string NewFilePath = $"{BoardFolder}/{SaveFileNameInput.Text}.txt";
+            SaveFileNameInput.Text = "";
+            FileManager.Save(NewFilePath,BoardContainer.Content);
+            FillLoadMenu();
+        }
+
+        private void AddNote_Click(object sender, RoutedEventArgs e)
+        {
+            AddNoteMode = true;
+        }
+
+        private void FillLoadMenu()
+        {
+            LoadBoardMenu.Items.Clear();
+            List<string> files = Directory.GetFiles(BoardFolder).ToList();
+            foreach (string file in files) {
+                string BoardName = Path.GetFileName(file).Replace(Path.GetExtension(file),"");
+                MenuItem BItem = new MenuItem();
+                BItem.Header = BoardName;
+                BItem.Click += BItem_Click;
+
+                LoadBoardMenu.Items.Add(BItem);
+            }
+        }
+
+        private void BItem_Click(object sender, RoutedEventArgs e)
+        {
+            string BoardPath = $"{BoardFolder}/{(sender as MenuItem).Header}.txt";
+            BoardContainer.Content = FileManager.Load<Canvas>(BoardPath);
+        }
+
+        private void ProjectView_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (AddNoteMode)
+            {
+                Point mc = e.GetPosition((Canvas)BoardContainer.Content);
+
+                // creating the note
+                Border NotePaper = new Border();
+                NotePaper.CornerRadius = new CornerRadius(20);
+                NotePaper.Background = new SolidColorBrush(Colors.Black);
+                NotePaper.BorderThickness = new Thickness(0);
+                NotePaper.Width = 200;
+                NotePaper.Height = 200;
+
+                Grid NoteFormat = new Grid();
+                NoteFormat.RowDefinitions.Add(new RowDefinition() {
+                    Height = new GridLength(0.2,GridUnitType.Star) 
+                });
+                NoteFormat.RowDefinitions.Add(new RowDefinition());
+                NoteFormat.ColumnDefinitions.Add(new ColumnDefinition());
+                NoteFormat.ColumnDefinitions.Add(new ColumnDefinition()
+                {
+                    Width = new GridLength(0.2, GridUnitType.Star)
+                });
+                NoteFormat.ColumnDefinitions.Add(new ColumnDefinition()
+                {
+                    Width = new GridLength(0.2, GridUnitType.Star)
+                });
+                NotePaper.Child = NoteFormat;
+
+                TextBox NoteTitle = new TextBox();
+                NoteTitle.Background = new SolidColorBrush(Colors.Transparent);
+                NoteTitle.Text = "Title";
+                NoteTitle.BorderBrush = new SolidColorBrush(Colors.Transparent);
+                NoteTitle.Foreground = new SolidColorBrush(Colors.LightSlateGray);
+                NoteTitle.FontWeight = FontWeights.Bold;
+                NoteTitle.Margin = new Thickness(10,5,0,0);
+                NoteTitle.FontSize = 20;
+                NoteFormat.Children.Add(NoteTitle);
+                Grid.SetRow(NoteTitle, 0);
+                Grid.SetColumn(NoteTitle, 0);
+
+                TextBox NotePassage = new TextBox();
+                NotePassage.Background = new SolidColorBrush(Colors.Transparent);
+                NotePassage.Text = "Enter Text...";
+                NotePassage.BorderBrush = new SolidColorBrush(Colors.Transparent);
+                NotePassage.Foreground = new SolidColorBrush(Colors.GhostWhite);
+                //NotePassage.FontWeight = FontWeights.Bold;
+                NotePassage.Margin = new Thickness(10);
+                NotePassage.FontSize = 15;
+                NotePassage.TextWrapping = TextWrapping.Wrap;
+                NoteFormat.Children.Add(NotePassage);
+                Grid.SetRow(NotePassage, 1);
+                Grid.SetColumnSpan(NotePassage, 3);
+                Grid.SetColumn(NotePassage, 0);
+
+                ((Canvas)BoardContainer.Content).Children.Add(NotePaper);
+                Canvas.SetTop(NotePaper, mc.Y);
+                Canvas.SetLeft(NotePaper, mc.X);
+
+                AddNoteMode = false;
+            }
+        }
+    }
+}
---
+++ PlanBoard/Tools/FileManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace PlanBoard.Tools
+{
+    public class FileManager
+    {
+        string connectionString = "Host=ep-young-fire-a81ppksr-pooler.eastus2.azure.neon.tech;Port=5432;Username=<neondb_owner>;Password=<npg_gmcuiq1ov0Qr>;Database=neondb?sslmode=require;SSL Mode=Require;Trust Server Certificate=true";
+        //postgresql://neondb_owner:npg_gmcuiq1ov0Qr@ep-young-fire-a81ppksr-pooler.eastus2.azure.neon.tech/neondb?sslmode=require
+
+        public FileManager()
+        {
+        }
+
+        public static void Save<T>(string path, T obj)
+        {
+            string mystrXAML = XamlWriter.Save(obj);
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+            File.WriteAllText(path, mystrXAML);
+        }
+
+        public static T Load<T>(string path)
+        {
+            string xamlString = File.ReadAllText(path);
+
+            using (var stringReader = new StringReader(xamlString))
+            {
+                using (var xmlReader = System.Xml.XmlReader.Create(stringReader))
+                {
+                    var obj = XamlReader.Load(xmlReader);
+                    return (T)obj;
+                }
+            }
+        }
+    }
+}
